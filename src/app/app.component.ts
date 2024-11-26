c0f9cf1bda49554bd689009f8693a2b542b90cbf
alan buscaglia <alanbuscaglia@gmail.com>
2024-11-26 17:42:47 +0100

    feat: upload project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormChildComponent } from './form-child/form-child.component';
+
+export interface ItemForm {
+  id: FormControl<number>;
+  name: FormControl<string>;
+  value: FormControl<number>;
+}
+
+export type CustomFormGroup = FormGroup<ItemForm>;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, FormChildComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-form';
+  fb = inject(NonNullableFormBuilder);
+
+  form: FormGroup<{ items: FormArray<CustomFormGroup> }> = this.fb.group({
+    items: this.fb.array<CustomFormGroup>([]),
+  });
+
+  get items() {
+    return this.form.controls.items;
+  }
+
+  itemChanges = toSignal(this.form.valueChanges);
+
+  totalValue = computed(() => {
+    const value = this.itemChanges()?.items?.reduce(
+      (total, item) => total + (Number(item?.value) || 0),
+      0,
+    );
+    console.log('computing total value: ', value);
+    return value;
+  });
+
+  addItem() {
+    const id = this.items.length + 1;
+    const itemForm = this.fb.group<ItemForm>({
+      id: this.fb.control(id),
+      name: this.fb.control('', { validators: [Validators.required] }),
+      value: this.fb.control(0, { validators: [Validators.required] }),
+    });
+
+    this.form.controls.items.push(itemForm);
+  }
 }
